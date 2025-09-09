a6b767126487bcfa8d673e4a0dfcb94b831af780
chaabane72 <chaabane72@yahoo.fr>
2025-09-09 08:42:45 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
 input.onButtonPressed(Button.A, function () {
-    Kitronik_LAMPbit.lampLightLED(Kitronik_LAMPbit.DisplayLamp.On)
-})
-input.onButtonPressed(Button.B, function () {
-    Kitronik_LAMPbit.lampLightLED(Kitronik_LAMPbit.DisplayLamp.On)
+	
 })
